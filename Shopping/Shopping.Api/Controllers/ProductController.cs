86fbbb51faa8f7797bafdf2fdf003536bad8ec3e
Gökhan ÖZTOPUZ <gokhanoztopuz@gmail.com>
2022-04-11 13:33:50 +0300

    ready1

---
+++ Shopping/Shopping.Api/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using Shopping.Api.Data;
+using Shopping.Api.Models;
+
+namespace Shopping.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly ProductContext _context;
+
+
+        public ProductController(ProductContext  context, ILogger<ProductController> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product2>> Get()
+        {
+      return await      _context.Products.Find(prop => true).ToListAsync();
+
+
+         //   return ProductContext.Products;
+
+        }
+
+
+    }
+}
---
+++ Shopping/Shopping.Api/Data/ProductContext.cs
@@ -0,0 +1,85 @@
+﻿using MongoDB.Driver;
+using Shopping.Api.Models;
+
+namespace Shopping.Api.Data
+{
+    public  class ProductContext
+    {
+
+        public IMongoCollection<Product2> Products { get; }
+
+        public ProductContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+            var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+            Products = database.GetCollection<Product2>(configuration["DatabaseSettings:CollectionName"]);
+
+    SeedData(Products);
+        }
+
+        private static void SeedData(IMongoCollection<Product2> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
+
+
+        private static IEnumerable<Product2> GetPreconfiguredProducts()
+        {
+            return new List<Product2>()
+            {
+                new Product2()
+                {
+                    Name = "IPhone X",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product2()
+                {
+                    Name = "Samsung 10",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-2.png",
+                    Price = 840.00M,
+                    Category = "Smart Phone"
+                },
+                new Product2()
+                {
+                    Name = "Huawei Plus",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-3.png",
+                    Price = 650.00M,
+                    Category = "White Appliances"
+                },
+                new Product2()
+                {
+                    Name = "Xiaomi Mi 9",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-4.png",
+                    Price = 470.00M,
+                    Category = "White Appliances"
+                },
+                new Product2()
+                {
+                    Name = "HTC U11+ Plus",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-5.png",
+                    Price = 380.00M,
+                    Category = "Smart Phone"
+                },
+                new Product2()
+                {
+                    Name = "LG G7 ThinQ EndofCourse",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-6.png",
+                    Price = 240.00M,
+                    Category = "Home Kitchen"
+                }
+            };
+        }
+    }
+}
